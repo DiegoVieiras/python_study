df6e7e3adce4f80b02a314370cf2b8ae1fff7476
DiegoVieiras <diegow.contato@gmail.com>
2025-03-20 15:04:28 +0000

    Estudo do livro dia 20/03

---
+++ Curso_Intensivo_Python/space.py
@@ -0,0 +1,11 @@
+#! /bin/env python3
+
+msg_space = "\n\tPython é \n\t\tdemais \n\t\t\tmesmo!"
+
+print(msg_space)
+
+space_py = " Python "
+
+print(space_py.strip())
+print(space_py.lstrip())
+print(space_py.rstrip())
